96614ec82889f3c47cb7ef8201673d90fb8a2f34
Ryan Mullin <rmullin7286@gmail.com>
2025-08-17 16:27:56 -0700

    Changed error name

--- src/main/java/me/rmullin/missing/control/Result.java
+++ src/main/java/me/rmullin/missing/control/Result.java
@@ -3,5 +3,5 @@ package me.rmullin.missing.control;
 public sealed interface Result<T, E> {
     record Ok<T, E>(T value) implements Result<T, E> {}
 
-    record Err<T, E>(E value) implements Result<T, E> {}
+    record Err<T, E>(E error) implements Result<T, E> {}
 }
